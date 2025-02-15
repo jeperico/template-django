1603bd9508421ac7888f7685f0d2f3d6a7ff6d78
João Périco <joaoeperico@gmail.com>
2025-02-14 23:18:00 -0300

    [feat] add cuser

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'rest_framework',
     # "rest_framework_simplejwt",
     # "rest_framework_simplejwt.token_blacklist",
+    "cuser",
     'drf_spectacular',
     'apps.profiles',
 ]
--- app/apps/profiles/models.py
+++ app/apps/profiles/models.py
@@ -1,10 +1,22 @@
-from django.db import models
+from cuser.models import AbstractCUser
 from utils.models import BaseModel
 
 
-class User(BaseModel):
+class User(AbstractCUser):
+  id = models.UUIDField(
+    primary_key=True,
+    default=uuid.uuid4,
+    editable=False,
+  )
   name = models.CharField(max_length=255)
   email = models.EmailField(unique=True)
+  created_at = models.DateTimeField(
+    auto_now_add=True,
+    editable=False,
+  )
+  updated_at = models.DateTimeField(
+    auto_now=True,
+  )
   # password = models.CharField(max_length=255)
 
   def __str__(self):
