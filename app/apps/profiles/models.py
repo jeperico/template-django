05492dc124bace26d8f834b569a899c9036bc052
João Périco <joaoeperico@gmail.com>
2025-02-14 22:47:55 -0300

    [feat] profiles: models, serializers && base

--- app/apps/profiles/models.py
+++ app/apps/profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from utils.models import BaseModel
 
-# Create your models here.
+
+class User(BaseModel):
+  name = models.CharField(max_length=255)
+  email = models.EmailField(unique=True)
+  # password = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name
---
+++ app/apps/profiles/serializers.py
@@ -0,0 +1,34 @@
+from .models import User
+from django.db import transaction
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = '__all__'
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+  password = serializers.CharField(
+    write_only=True,
+    required=True,
+    validators=[validate_password],
+  )
+  
+  class Meta:
+    model = User
+    fields = ['id', 'name', 'email', 'password', 'created_at', 'updated_at'], 
+    extra_kwargs = {
+      'password': {'write_only': True},
+      'id': {'read_only': True},
+    }
+  
+  def create(self, validated_data):
+    with transaction.atomic():
+      password = validated_data.pop('password')
+      user = User.objects.create(**validated_data)
+      user.set_password(password)
+      user.save()
+      return user
