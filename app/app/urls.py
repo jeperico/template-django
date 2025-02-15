5f947bd4d2b774d4aca047a69e3a93cd7a9da75f
João Périco <joaoeperico@gmail.com>
2025-02-14 21:15:41 -0300

    [fix] import and comma

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v2/", include("urls")),
+    path("api/v1/", include("urls")),
 ]
--- app/urls.py
+++ app/urls.py
@@ -9,20 +9,20 @@ urlpatterns = [
   path(
     'schema/',
     SpectacularAPIView.as_view(),
-    name='schema'
-  )
+    name='schema',
+  ),
   path(
     'schema/swagger/',
     SpectacularSwaggerView.as_view(url_name='schema'),
     name='swagger-ui'
-  )
+  ),
   path(
     'schema/redoc/',
     SpectacularRedocView.as_view(url_name='schema'),
     name='redoc'
-  )
+  ),
   # path(
   #   'profiles/',
   #   include('apps.profiles.urls')
-  # )
+  # ),
 ]
