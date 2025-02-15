018e3830469d7090d418bd32837baf66ab3b9a85
João Périco <joaoeperico@gmail.com>
2025-02-14 23:00:21 -0300

    [feat] profiles: admin && urls

--- app/apps/profiles/admin.py
+++ app/apps/profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+  list_display = ['id', 'name', 'email', 'created_at', 'updated_at']
+  search_fields = ['name', 'email']
+  list_filter = ['created_at', 'updated_at']
+  readonly_fields = ['created_at', 'updated_at']
+
+  admin.site.register(User, UserAdmin)
